34a5836ed397d90eba28b79406542d5e343742b8
李姿轩 <965555169@qq.com>
2021-02-20 19:12:18 +0800

    订单功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -40,6 +40,9 @@ import {
     Upload
 } from 'element-ui'
 
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
+
 
 Vue.use(Button)
 Vue.use(Form)
@@ -86,6 +89,8 @@ Vue.use(Checkbox)
 Vue.use(Upload)
 
 
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 
 

c7c840f0a36bad594e3319f9d0c892a32c7e6f8b
李姿轩 <965555169@qq.com>
2021-02-23 16:11:48 +0800

    项目优化

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,24 @@
+// 项目发布阶段需要用到的babel插件
+const proPlugins = []
+if (process.env.NODE_ENV === 'production') {
+    proPlugins.push('transform-remove-console')
+}
+
+
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    [
-      'component',
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk'
-      }
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    plugins: [
+        [
+            'component',
+            {
+                libraryName: 'element-ui',
+                styleLibraryName: 'theme-chalk'
+            }
+        ],
+        // 发布时的插件
+        ...proPlugins,
+        '@babel/plugin-syntax-dynamic-import'
     ]
-  ]
 }
--- src/main-prod.js
+++ src/main-prod.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// import './plugins/element.js'
 // 导入全局样式
 import './assets/css/global.css'
 // 导入字体图标
@@ -11,22 +11,33 @@ import TreeTable from 'vue-table-with-tree-grid'
 
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-// 导入富文本编辑器样式
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+
+// 导入Nprogress
+import Nprogress from 'nprogress'
+
+
+
 
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
 
+// 在request拦截器中展示进度条
 // 通过axios请求拦截器添加token,保证拥有获取数据的权限
 axios.interceptors.request.use(config => {
     // console.log(config);
+    Nprogress.start();
     config.headers.Authorization = window.sessionStorage.getItem('token');
     // 在最后必须return config
     return config;
 });
+
+// 在response拦截器中隐藏进度条
+axios.interceptors.response.use(config => {
+    Nprogress.done();
+    return config;
+})
+
 
 Vue.prototype.$http = axios;
 
